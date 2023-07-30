22623cd7f7bd84d6212f5bae8aeee0bf709be128
Shakiful <shakifulkhan@gmail.com>
2023-07-30 15:51:33 +0600

    changed default page load to auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/auth', pathMatch: 'full' },
   { path: 'home', loadChildren:() => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'auth', loadChildren:() => import('./auth/auth.module').then(m => m.AuthModule) },
   // { path: 'about', loadChildren:() => import('./auth/auth.module').then(m => m.AuthModule) },
