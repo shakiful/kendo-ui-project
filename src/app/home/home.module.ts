6f713f079ed508c7a20e52c74e36879c791ab7d5
Shakiful <shakifulkhan@gmail.com>
2023-08-01 18:00:34 +0600

    logout added

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { LayoutModule } from '@progress/kendo-angular-layout';
+import { GridModule } from '@progress/kendo-angular-grid';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [RouterModule,HomeRoutingModule, LayoutModule],
+  imports: [RouterModule,HomeRoutingModule, LayoutModule, GridModule],
 })
 export class HomeModule {}
